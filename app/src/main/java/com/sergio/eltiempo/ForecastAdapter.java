18b88b185455e1018d2e21b129a2fbdb32b47f18
Sergio <sergiotmg@gmail.com>
2019-02-06 10:27:03 +0100

    version completa y con redondeo sobra el WeatherAdapter

--- app/src/main/java/com/sergio/eltiempo/ForecastAdapter.java
+++ app/src/main/java/com/sergio/eltiempo/ForecastAdapter.java
@@ -1,9 +1,11 @@
 package com.sergio.eltiempo;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,33 +13,40 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.List;
 public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastViewHolder> {
-    private List<Forecast> mForecastList;
-    //private List<Forecast> mForecastList;
+    private List<Forecast> forecastList;
+    //private List<Forecast> forecastList= new ArrayList<>(); nunca sera vacia y
+    // luego en el getItemCount()  no hace falta esto: if (forecastList==null) return  0;
     private LayoutInflater mInflater;
-    //private Context context;
+    private Context context;
 
 
         //CONSTRUCTOR
+        // podemos no pasrle la List por ahi
         public ForecastAdapter(Context context, List<Forecast> weatherList) {//context no se deberia
-            mInflater = LayoutInflater.from(context);// si hay 20 creara 20
-            this.mForecastList = weatherList;
-            //this.mForecastList = forecastList;
-            //this.context=context;// no necesaria , podemos eliminar arriba private Context context
+            mInflater = LayoutInflater.from(context);// los inflará
+            this.forecastList = weatherList;
+            //this.forecastList = forecastList;
+            this.context=context;// no necesaria , podemos eliminar arriba private Context context
 
         }
+        // si declaro la clase abstracta, no hara falta implementar todos los methods
     class ForecastViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener { // NUNCA CAMBIA ESTA FRASE
         public final ConstraintLayout mConstraintLayout;
 
-        // etiquetas del forecast_item
-        public final ImageView mImgState;
-        public final TextView  mTxtDate;
-        public final TextView mTxtWindDirection;
-        public final TextView mTxtMaxTemp;
-        public final TextView mTxtMinTemp;
-        public final TextView mTxtWindSpeed;
-        public final TextView mTxtActTemp;
+        // etiquetas del forecast_item, al ser final son ctes
+        public final ImageView ImgState;
+        public final TextView TxtDate;
+        public final TextView TxtWindDirection;
+        public final TextView TxtMaxTemp;
+        public final TextView TxtMinTemp;
+        public final TextView TxtWindSpeed;
+        public final TextView TxtActTemp;
 
         // etiquetas del activity_main
         public final TextView  mTxtTitle;
@@ -47,17 +56,14 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
 
         // constructor para el ForecastViewHolder
         public ForecastViewHolder(View itemView,ForecastAdapter adapter) {
-
             super(itemView);
-
-
-            mImgState=itemView.findViewById(R.id.imgState);
-            mTxtDate=itemView.findViewById(R.id.txtDate);
-            mTxtWindDirection=itemView.findViewById(R.id.txtWindDirection);
-            mTxtMinTemp =itemView.findViewById(R.id.txtMinTemp);
-            mTxtMaxTemp =itemView.findViewById(R.id.txtMaxTemp);
-            mTxtWindSpeed=itemView.findViewById(R.id.txtWindSpeed);
-            mTxtActTemp=itemView.findViewById(R.id.txtActTemp);
+            ImgState=itemView.findViewById(R.id.imgState);
+            TxtDate=itemView.findViewById(R.id.txtDate);
+            TxtWindDirection=itemView.findViewById(R.id.txtWindDirection);
+            TxtMinTemp =itemView.findViewById(R.id.txtMinTemp);
+            TxtMaxTemp =itemView.findViewById(R.id.txtMaxTemp);
+            TxtWindSpeed=itemView.findViewById(R.id.txtWindSpeed);
+            TxtActTemp=itemView.findViewById(R.id.txtActTemp);
 
             mTxtTitle=itemView.findViewById(R.id.txtTitle);
 
@@ -72,7 +78,7 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
         @Override
         public void onClick(View v) {
             int mPosition = getLayoutPosition();
-            //Forecast element = mForecastList.get(mPosition);
+            //Forecast element = forecastList.get(mPosition);
         }
     }
 
@@ -80,30 +86,52 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
     @Override
     public ForecastAdapter.ForecastViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
         View mItemView=mInflater.inflate(R.layout.forecast_item,parent,false);
+        //(................ , .............. , .........)
+        //el layout, donde lo vamos a cargar y si queremos attacherarlo a el( false porque nunca se fija al padre
+        // generamos una variable y la devovlemos , en este caso directamente
         return new ForecastViewHolder(mItemView,this);
+        //return new ForecastViewHolder(mItemView); Nacho lo pone sin this
     }
 
     @Override
     public void onBindViewHolder(@NonNull ForecastAdapter.ForecastViewHolder holder, int position) {
-        Forecast mCurrent = mForecastList.get(position);
+        Forecast mCurrent = forecastList.get(position);
 
         // coger la imagen de la clase Forecast NO SE COMO SE HACE
         //holder.mImgState.setImageResource(mCurrent.getConsolidated_weather());
+        String url="https://www.metaweather.com/static/img/weather/png/64/"+mCurrent.getWeather_state_abbr()+".png";
+        Glide.with(context).load(url).into(holder.ImgState);
 
-        holder.mTxtActTemp.setText(mCurrent.getThe_temp().intValue());
-        holder.mTxtMaxTemp.setText(mCurrent.getMax_temp().intValue());
-        holder.mTxtMinTemp.setText(mCurrent.getMin_temp().intValue());
-        holder.mTxtWindDirection.setText(mCurrent.getWind_direction_compass());
-        holder.mTxtWindSpeed.setText(mCurrent.getWind_speed().intValue());
-        holder.mTxtDate.setText(mCurrent.getApplicable_date());
+        holder.TxtActTemp.setText(get1DecimalNumber(mCurrent.getThe_temp())+" ºC");
 
+        holder.TxtMaxTemp.setText(get1DecimalNumber(mCurrent.getMax_temp())+" ºC");
+        holder.TxtMinTemp.setTextColor(Color.RED);
+
+        holder.TxtMinTemp.setText(get1DecimalNumber(mCurrent.getMin_temp())+" ºC");
+        holder.TxtMinTemp.setTextColor(Color.BLUE);
+
+        holder.TxtWindDirection.setText(mCurrent.getWind_direction_compass());
+        holder.TxtWindSpeed.setText(get1DecimalNumber(mCurrent.getWind_speed())+" Km/h");
+        holder.TxtDate.setText(mCurrent.getApplicable_date());
+        //holder.mTxtTitle.setText(mCurrent.get);
+        Log.d("weather ADAPTER:",url);
         holder.mConstraintLayout.setBackgroundColor(9);
     }
 
-
     @Override
     public int getItemCount() {
-        return mForecastList.size();
+        if (forecastList==null) return  0;
+        return forecastList.size();
+    }
+
+     public String get1DecimalNumber(float number){
+         DecimalFormat df=new DecimalFormat("#.#");
+         df.setRoundingMode(RoundingMode.CEILING.HALF_EVEN);
+         return df.format(number);
+     }
+    public void setData(List<Forecast> newList){
+            forecastList=newList;
+            notifyDataSetChanged();//actualiza toda la lista entera
     }
 
 
--- app/src/main/java/com/sergio/eltiempo/MainActivity.java
+++ app/src/main/java/com/sergio/eltiempo/MainActivity.java
@@ -4,43 +4,48 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements Controller.ServerResponse {
     private Weather mWeather;
-    private List<Forecast> consolidatedWeather;
-    private RecyclerView mRecyclerView;
-    private  ForecastAdapter mForecastAdapter;
-    private  WeatherAdapter mWeatherAdapter;
+    //private List<Forecast> consolidatedWeather;
+    private RecyclerView RecyclerView;
+    private  ForecastAdapter adapter;
+    //private  WeatherAdapter WeatherAdapter;
+    private TextView txtTitle;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtTitle=findViewById(R.id.txtTitle);
+
         Controller controller= new Controller(this);
         controller.start();
-        consolidatedWeather=mWeather.getConsolidated_weather();
-        
-        mRecyclerView=findViewById(R.id.recyclerView);
-        mForecastAdapter=new ForecastAdapter(this,consolidatedWeather);
-        
-        
+        //consolidatedWeather=mWeather.getConsolidated_weather(); ESO va abajo en On Response
+        RecyclerView=findViewById(R.id.recyclerView);
+        //Adapter=new Adapter(this,consolidatedWeather);
+        adapter=new ForecastAdapter(this,null);
+        RecyclerView.setAdapter(adapter);
         
         LinearLayoutManager manager=new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(manager);
+        RecyclerView.setLayoutManager(manager);
+
+
 
 
     }
     // necesitamos qwue nos pase la respuesta . Es oblig implementaro
     @Override
     public void onResponse(Weather weather) {//llegara un weather cada vez
-        weather.getConsolidated_weather();
+        //weather.getConsolidated_weather();
         // la lista del getconsolidated_weather es lo que hay que pasrle al recicylerView
-        Toast.makeText(this,weather.getTitle(),Toast.LENGTH_LONG).show();
-
-
+        //Toast.makeText(this,weather.getTitle(),Toast.LENGTH_LONG).show();
+        adapter.setData(weather.getConsolidated_weather());
+        txtTitle.setText(weather.getTitle());
 
     }
 
